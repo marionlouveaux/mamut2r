a91f4da8cdd166186d00308425d69782610d8209
mlouveaux <marion.louveaux@cos.uni-heidelberg.de>
2018-09-24 15:42:50 +0200

    Moving hex2int from vignette to .R

---
+++ R/hex2int.R
@@ -0,0 +1,19 @@
+#' Converting HEX colors to MaMut integer color scale
+#'
+#' @param x an HEX color
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+hex2int <- function(x) {
+  myRGBA <- c(col2rgb(x), 255)
+  HEX_ord <- rgb(myRGBA[3], myRGBA[2], myRGBA[1], myRGBA[4], max = 255)
+  col2rgb(HEX_ord, alpha = TRUE) %>%
+    as.raw() %>%
+    rawToBits() %>%
+    packBits(type = "integer")
+}
+
+
