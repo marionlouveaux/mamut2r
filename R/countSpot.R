0ef219cc7b653cfca4c9249b98c21b4182032e4c
mlouveaux <marion.louveaux@cos.uni-heidelberg.de>
2018-09-24 15:41:59 +0200

    Moving countSpot from vignette to .R

---
+++ R/countSpot.R
@@ -0,0 +1,24 @@
+#' Counting spots per timepoints
+#'
+#' @param Spots_df a dataframe created with Spots.as.Dataframe
+#' @param timepoint column of timepoints
+#' @importFrom dplyr enquo
+#' @importFrom dplyr group_by
+#' @importFrom left_join
+#' @importFrom dplyr summarize
+#'
+#' @return
+#' @export
+#'
+#' @examples
+countSpot <- function(Spots_df, timepoint) {
+
+  timepoint <- enquo(timepoint)
+  nb_nuclei <- Spots_df %>%
+    group_by((!!timepoint)) %>%
+    summarize(nb = n())
+
+  left_join(Spots_df, nb_nuclei)
+}
+
+
