d6d953698eb2ddcc837a545086215aac2c8ab827
mlouveaux <marion.louveaux@cos.uni-heidelberg.de>
2018-09-24 15:43:43 +0200

    Moving mmFluo from vignette to .R

---
+++ R/mmFluo.R
@@ -0,0 +1,31 @@
+#' Calculate a mean and median fluorescent value for each cell
+#'
+#' @param Spots_df a dataframe containing the spots coordinates
+#' @param all_cells fluorescent values collected in a cube of a given size around each spot location
+#' @importFrom dplyr left_join
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+mmFluo <- function(Spots_df, all_cells) {
+
+  mmFluo <- lapply(all_cells, function(x){
+    oneFrame <- lapply(x, function(y){
+      data.frame(frame = as.character(y[[1]]),
+                 name = y[[2]],
+                 meanFluo = mean(y[[3]]), # mean fluo value per spot
+                 medianFluo = median(y[[3]])
+      )
+    })
+    do.call(rbind.data.frame, oneFrame)
+  })
+
+  df_meanFluo <- do.call(rbind.data.frame, mmFluo)
+  all_data <- left_join(x = Spots_df, y = df_meanFluo)
+
+  return(all_data)
+}
+
+
