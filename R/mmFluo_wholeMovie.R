5863b777e11a7e8564be4de41a0d94300efc9eb8
mlouveaux <marion.louveaux@cos.uni-heidelberg.de>
2018-09-24 15:44:28 +0200

    Moving mmFluo_wholeMovie from vignette to .R

---
+++ R/mmFluo_wholeMovie.R
@@ -0,0 +1,25 @@
+#' Calculate a mean and median fluorescent value for the whole movie over all spots
+#'
+#' @param Spots_df a dataframe containing the spots coordinates
+#' @param all_cells fluorescent values collected in a cube of a given size around each spot location
+#' @importFrom dplyr mutate
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+mmFluo_wholeMovie <- function(Spots_df, all_cells) {
+
+  fluo_perT <- lapply(all_cells, function(time){
+    unlist(lapply(time, function(x){x[[3]]}))
+  })
+
+  Spots_df %>%
+    mutate(meanFluo_wholeMovie = mean(unlist(fluo_perT)),
+           medianFluo_wholeMovie = median(unlist(fluo_perT))
+    )
+}
+
+
+
