1804fcbe9dba721ad4cc5c51d91c9450a496e298
mlouveaux <marion.louveaux@cos.uni-heidelberg.de>
2018-09-24 15:42:36 +0200

    Moving heatmap2int from vignette to .R

---
+++ R/heatmap2int.R
@@ -0,0 +1,28 @@
+## convert df_meanFluo: heatmap color then HEX then MaMuT color
+
+#' Conversion of continuous variable to discrete integers for visualisation in MaMuT
+#'
+#' @param Spots_df
+#' @param my_var continuous variable to convert as discrete HEX, then integer readable by MaMuT
+#' @importFrom dplyr enquo
+#' @importFrom dplyr mutate
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+heatmap2int <- function(Spots_df, my_var = fluoMean) {
+  my_var <- enquo(my_var)
+  Spots_df <- Spots_df %>%
+    mutate(colorClass = as.numeric(cut( (!! my_var),
+                                        breaks = classInt::classIntervals( (!! my_var),
+                                                                           n = 6,
+                                                                           style = "pretty")$brks))) %>%
+    mutate(heat = viridis(n = 6)[colorClass]) %>%
+    mutate(MANUAL_COLOR = purrr::map(heat, hex2int) )
+  return(Spots_df)
+}
+
+
+
