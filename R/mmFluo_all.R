48d1b09f007705965177cb484114837228dda296
mlouveaux <marion.louveaux@cos.uni-heidelberg.de>
2018-09-24 15:44:00 +0200

    Moving mmFluo_all from vignette to .R

---
+++ R/mmFluo_all.R
@@ -0,0 +1,18 @@
+#' Calculate mean and median fluorescent values for individual cells, per timepoin and for the whole movie
+#'
+#' @param Spots_df  a dataframe containing the spots coordinates
+#' @param all_cells fluorescent values collected in a cube of a given size around each spot location
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+mmFluo_all <- function(Spots_df, all_cells) {
+
+  Spots_df %>%
+    mmFluo(all_cells) %>%
+    mmFluo_perT(all_cells) %>%
+    mmFluo_wholeMovie(all_cells) %>%
+    diffFluo(all_cells, meancol = meanFluo)
+}
