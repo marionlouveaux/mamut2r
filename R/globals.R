49e7289d4fa7d6e2bfc56317fc4323dcf8ce8987
mlouveaux <marion.louveaux@cos.uni-heidelberg.de>
2018-09-24 09:37:55 +0200

    Adding global variables.

---
+++ R/globals.R
@@ -0,0 +1,12 @@
+
+utils::globalVariables(c("ID",
+                         "RADIUS",
+                         "POSITION_T",
+                         "POSITION_X",
+                         "POSITION_Y",
+                         "POSITION_Z",
+                         "SPOT_SOURCE_ID",
+                         "SPOT_TARGET_ID",
+                         "VELOCITY",
+                         "DISPLACEMENT"
+                         ))
