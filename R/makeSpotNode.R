252c9755b72ee5520f7304b394b206ec00ac8674
mlouveaux <marion.louveaux@cos.uni-heidelberg.de>
2018-09-24 15:43:31 +0200

    Moving makeSpotNode from vignette to .R

---
+++ R/makeSpotNode.R
@@ -0,0 +1,16 @@
+#' Create a spot node
+#' To modify the mamut.xml file.
+#'
+#' see also documentation here: https://cran.r-project.org/web/packages/xml2/vignettes/modification.html
+#' @param ... A spot dataframe
+#'
+#' @return
+#' @export
+#'
+#' @examples
+makeSpotNode <- function(...) {
+  x <- unlist(list(...))
+  paste0("Spot ", paste(paste0(names(x), '="', x, '"'), collapse = " "))
+}
+
+
