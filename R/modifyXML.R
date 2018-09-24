946ed49d93ed73afffdbff1f507005f6d3208691
mlouveaux <marion.louveaux@cos.uni-heidelberg.de>
2018-09-24 15:44:45 +0200

    Moving modifyXML from vignette to .R

---
+++ R/modifyXML.R
@@ -0,0 +1,39 @@
+#' Modify MaMuT .xml file
+#' Update values of existing parameters and/or add the manual color parameter
+#'
+#' @param MaMuTpath path to an existing MaMuT .xml file
+#' @param newFilename new name for the modified .xml
+#' @param Spots_df spot dataframe corresponding to the MaMuT .xml file, with or without the MANUAL_COLOR variable
+#' @importFrom dplyr select_if
+#' @importFrom purrr pmap_chr
+#' @importFrom xml2 xml_replace
+#' @importFrom xml2 xml_find_all
+#' @importFrom xml2 write_xml
+#'
+#' @return a new .xml file
+#' @export
+#'
+#' @examples
+
+#NB: I should do the select based on the Spot and Edges features
+modifyXML <- function(MaMuTpath,
+                      newFilename = "test.xml",
+                      Spots_df) {
+  x <- read_xml(MaMuTpath)
+
+  Spots_df_tmp <- Spots_df %>%
+    select_if( names(Spots_df) %in% c("ID", "name",
+                                      "VISIBILITY", "RADIUS",
+                                      "QUALITY", "SOURCE_ID",
+                                      "POSITION_T", "POSITION_X",
+                                      "POSITION_Y", "FRAME",
+                                      "POSITION_Z",
+                                      "MANUAL_COLOR"))
+
+  myNode <- xml_find_all(x, './/Spot')
+  xml_replace(myNode, pmap_chr(Spots_df_tmp, makeSpotNode))
+  write_xml(x, file = newFilename)
+}
+
+
+
