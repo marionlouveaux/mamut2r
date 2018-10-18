a5e85acb2b917c8e6ab6360ae4d43c81e7e15b01
marionlouveaux <marion.louveaux@gmail.com>
2018-10-18 12:56:01 +0200

    posLoc2posWorld: new function.

---
+++ R/posLoc2posWorld.R
@@ -0,0 +1,15 @@
+#' Calculating the coordinates
+#' N.B.: pos_world = (3D-HASHING) x (CALIBRATION) x pos_local
+#'
+#' @param pos_local Coordinates in the local coordinates system of MaMuT Viewer (POSITION_X, POSITION_Y, POSITION_Z) in arbitrary units.
+#' @param affTransform 4x4 affine transform from local to world coordinates.
+#'
+#' @return Coordinates corresponding to the .h5 raw data pixels
+#' @export
+#'
+#' @examples
+posLoc2posWorld <- function(pos_local, affTransform) {
+  pos_local <- c(pos_local, 1) # we want to transform a point
+  pos_world <- affTransform %*% pos_local
+  return(pos_world)
+}
