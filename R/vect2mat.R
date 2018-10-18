9128dc65facf049befa33ebb50496677912de0fb
marionlouveaux <marion.louveaux@gmail.com>
2018-10-18 12:56:44 +0200

    vect2mat: new function.

---
+++ R/vect2mat.R
@@ -0,0 +1,15 @@
+#' Takes a 12 coordinates vector and returns a 4x4 matrix
+#'
+#' @param affine_vect affine transform vector with 12 indices. The vector store the “top” three rows for the 4x4 affine matrix, in a "row major" fashion.
+#'
+#' @return a 4x4 affine transform matrix, with the top three rows and a last row containing 0,0,0,1
+#' @export
+#'
+#' @examples
+#' vect2mat(1:12)
+vect2mat <- function(affine_vect) {
+  # mat_tmp <- matrix(data = affine_vect, nrow = 3, ncol = 4, byrow = TRUE)
+  # affine_mat <- rbind(mat_tmp, c(0,0,0,1))
+  affine_mat <- matrix(data = c(affine_vect, 0,0,0,1), nrow = 3, ncol = 4, byrow = TRUE)
+  return(affine_mat)
+}
